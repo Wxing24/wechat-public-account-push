37975d95dbafff5819b73daf1b7b5684ea26ebba
Wxing24 <wangxg024@outlook.com>
2022-09-21 18:21:00 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -4,13 +4,9 @@ export const config = {
    */
 
   // 公众号APP_ID
-  // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
-  // 如果你非要填这里也行。脚本也能运行
   APP_ID: "",
 
   // 公众号APP_SECRET
-  // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
-  // 如果你非要填这里也行。脚本也能运行
   APP_SECRET: "",
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
@@ -87,7 +83,7 @@ export const config = {
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: [
         // 在一起的日子
-        {"keyword": "love_day", date: "2022-09-08"},
+        {"keyword": "love_day", date: "2022-01-13"},
         // 结婚纪念日
         //{"keyword": "marry_day", date: "2022-09-09"},
         // 退伍日
@@ -99,12 +95,14 @@ export const config = {
       id: "o5qb15nph-do8MvmvsoBD0kUTJ3o",
       useTemplateId: "m_jd39t3RZGpaFmwja-_CFIyCo0Bfle_q-MJsH5dRhA",
       province: "甘肃",
-      city: "民勤",
+      city: "武威",
       horoscopeDate: '12-2',
       horoscopeDateType: '今日',
-      openUrl: "https://wangxinleo.cn",
+      openUrl: "https://s.weibo.com/top/summary?cate=realtimehot",
       festivals: [],
-      customizedDateList: []
+      customizedDateList: [
+          {"keyword": "love_day", date: "2022-01-13"},
+      ]
     },
 
   ],
